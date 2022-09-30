2aca1a58a96db2683b9463ff4834263a26edfe9a
afiq <afiqvdx20@gmail.com>
2022-09-30 21:18:18 +0800

    Fix Error for Prtoect Admin Dashboard

--- routes/web.php
+++ routes/web.php
@@ -30,11 +30,11 @@ Route::group(['prefix'=> 'admin', 'middleware'=>['admin:admin']], function(){
 	Route::post('/login',[AdminController::class, 'store'])->name('admin.login');
 });
 
-
-
+Route::middleware(['auth:admin'])->group(function(){
 
 Route::middleware(['auth:sanctum,admin', 'verified'])->get('/admin/dashboard', function () {
     return view('admin.index');
+
 })->name('dashboard');
 // All admin routes (Jerat Percintaan)
 Route::get('/admin/logout', [AdminController::class, 'destroy'])->name('admin.logout');
@@ -48,20 +48,6 @@ Route::post('/admin/profile/store', [AdminProfileController::class, 'AdminProfil
 Route::get('/admin/change/password', [AdminProfileController::class, 'AdminChangePassword'])->name('admin.change.password');
 
 Route::post('/update/change/password', [AdminProfileController::class, 'AdminUpdateChangePassword'])->name('update.change.password');
-
-
-// USER ALL ROUTES
-Route::middleware(['auth:sanctum,web', 'verified'])->get('/dashboard', function () {
-    return view('frontend.dashboard');
-})->name('dashboard');
-
-Route::get('/', [IndexController::class, 'index']);
-Route::get('/user/logout', [IndexController::class, 'UserLogout'])->name('user.logout');
-Route::get('/user/profile', [IndexController::class, 'UserProfile'])->name('user.profile');
-Route::post('/user/profile/store', [IndexController::class, 'UserProfileStore'])->name('user.profile.store');
-Route::get('/user/profile/update-password', [IndexController::class, 'UserChangePassword'])->name('user.change.password');
-Route::post('/user/password/update', [IndexController::class, 'UserPasswordUpdate'])->name('user.password.update');
-
 
 //Admin All Brand Route
 Route::prefix('brand')->group(function(){
@@ -138,3 +124,23 @@ Route::prefix('slider')->group(function(){
     Route::get('/active/{id}', [SliderController::class, 'SliderActive'])->name('slider.active');
 
 });
+
+
+});// admin middleware
+
+
+
+// USER ALL ROUTES
+Route::middleware(['auth:sanctum,web', 'verified'])->get('/dashboard', function () {
+    return view('frontend.dashboard');
+})->name('dashboard');
+
+Route::get('/', [IndexController::class, 'index']);
+Route::get('/user/logout', [IndexController::class, 'UserLogout'])->name('user.logout');
+Route::get('/user/profile', [IndexController::class, 'UserProfile'])->name('user.profile');
+Route::post('/user/profile/store', [IndexController::class, 'UserProfileStore'])->name('user.profile.store');
+Route::get('/user/profile/update-password', [IndexController::class, 'UserChangePassword'])->name('user.change.password');
+Route::post('/user/password/update', [IndexController::class, 'UserPasswordUpdate'])->name('user.password.update');
+
+
+
