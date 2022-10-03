dca8b6c3b3b4c85913ecb658efe8805dec39dd03
afiq <afiqvdx20@gmail.com>
2022-10-03 15:40:55 +0800

    Frontend Dynamics Bootstrap Tabs 1

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Models\User;
 use App\Models\Slider;
+use App\Models\Product;
 use App\Models\Category;
 use App\Models\SubCategory;
 use Illuminate\Http\Request;
@@ -15,9 +16,10 @@ use Illuminate\Support\Facades\Hash;
 class IndexController extends Controller
 {
     public function index(){
-        $sliders = Slider::where('status',1)->orderBy('id', 'DESC')->get();
+        $products = Product::where('status', 1)->orderBy('id', 'DESC')->get();
+        $sliders = Slider::where('status',1)->orderBy('id', 'DESC')->limit(3)->get();
         $categories = Category::orderBy('category_name_en', 'ASC')->get();
-        return view('frontend.index', compact('categories','sliders'));
+        return view('frontend.index', compact('categories','sliders','products'));
     }
 
     public function UserLogout(){
