fa169e7001351bc54fcc403e0d76828e9ed910e0
afiq <afiqvdx20@gmail.com>
2022-10-06 15:32:29 +0800

    Product Show With Skip Category & Brand Part 2

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Models\User;
+use App\Models\Brand;
 use App\Models\Slider;
 use App\Models\Product;
 use App\Models\Category;
@@ -27,9 +28,16 @@ class IndexController extends Controller
 
         $skip_category_0 = Category::skip(0)->first();
         $skip_product_0 = Product::where('status', 1)->where('category_id', $skip_category_0->id)->orderBy('id', 'DESC')->get();
+
+        $skip_category_1 = Category::skip(1)->first();
+        $skip_product_1 = Product::where('status', 1)->where('category_id', $skip_category_1->id)->orderBy('id', 'DESC')->get();
+
+        $skip_brand_1 = Brand::skip(1)->first();
+        $skip_brand_product_1 = Product::where('status', 1)->where('brand_id', $skip_brand_1->id)->orderBy('id', 'DESC')->get();
         // return $skip_category->id;
         // die();
-        return view('frontend.index', compact('categories','sliders','products','featured','hot_deals','special_offer','special_deals','skip_category_0','skip_product_0'));
+        return view('frontend.index', compact('categories','sliders','products','featured','hot_deals','special_offer','special_deals','skip_category_0','skip_product_0','skip_category_1','skip_product_1',
+        'skip_brand_1','skip_brand_product_1'));
     }
 
     public function UserLogout(){
