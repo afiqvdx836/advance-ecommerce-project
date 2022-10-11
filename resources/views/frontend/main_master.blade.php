6ac583117573cef059f0ad7ca1b614f9da3da9ac
afiq <afiqvdx20@gmail.com>
2022-10-11 13:33:18 +0800

    Product View Modal With Ajax Part 4

--- resources/views/frontend/main_master.blade.php
+++ resources/views/frontend/main_master.blade.php
@@ -94,7 +94,7 @@
 	<div class="modal-dialog" role="document">
 	  <div class="modal-content">
 		<div class="modal-header">
-		  <h5 class="modal-title" id="exampleModalLabel"><span class="" id="pname">P</span></h5>
+		  <h5 class="modal-title" id="exampleModalLabel"><strong><span class="" id="pname">P</span></strong></h5>
 		  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
 			<span aria-hidden="true">&times;</span>
 		  </button>
@@ -113,11 +113,17 @@
 
 				<div class="col-md-4">
 					<ul class="list-group">
-						<li class="list-group-item">Product Price: <strong id="price"></strong> </li>
+						<li class="list-group-item">Product Price: <strong class="text-danger">$<span id="pprice"></span></strong> 
+							<del id="oldprice"></del>
+						</li>
 						<li class="list-group-item">Product Code: <strong id="pcode"></strong></li>
 						<li class="list-group-item">Category: <strong id="pcategory"></strong></li>
 						<li class="list-group-item">Brand: <strong id="pbrand"></strong></li>
-						<li class="list-group-item">Stock:  <strong id="pstock"></strong></li>
+						<li class="list-group-item">Stock: 
+							<span class="badge badge-pill badge-success" id="available" style="background: green; color:white;"></span>
+							<span class="badge badge-pill badge-danger" id="stockout" style="background: red; color:white;"></span>
+						 </li>
+					
 					  </ul>
 				</div> <!-- End col-md-4 -->
 
@@ -173,9 +179,22 @@ function productView(id){
 			$('#pname').text(data.product.product_name_en);
 			$('#price').text(data.product.selling_price);
 			$('#pcode').text(data.product.product_code);
-			$('#pbrand').text(data.product.brand.brand_name_en);
 			$('#pcategory').text(data.product.category.category_name_en);
+			$('#pbrand').text(data.product.brand.brand_name_en);
 			$('#pimage').attr('src','/'+data.product.product_thambnail);
+
+			//Product Price
+			if (data.product.discount_price == null){
+				$('#pprice').text('');
+				$('#oldprice').text('');
+				$('#pprice').text(data.product.selling_price);
+			
+			}else{
+				$('#pprice').text(data.product.discount_price);
+				$('#oldprice').text(data.product.selling_price);
+			} // end product price
+
+		
 
 			//color
 			$('select[name="color"]').empty();
@@ -194,7 +213,26 @@ function productView(id){
 					$('#sizeArea').show();
 				}
 			
-			})
+			})// end size
+
+			//stockout
+			
+			if(data.product.product_qty > 0){
+				$('#available').text('');
+				$('#stockout').text('');
+				$('#available').text('available');
+			}else{
+				$('#available').text('');
+				$('#stockout').text('');
+				$('#stockout').text('stockout');
+			}
+
+
+			//stock
+
+			
+
+
         }
     })
  
