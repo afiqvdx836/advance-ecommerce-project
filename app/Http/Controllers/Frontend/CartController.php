1528c5d133585feb347d93067934daefa277ba03
afiq <afiqvdx20@gmail.com>
2022-10-15 07:18:37 +0800

    Wishlist Product Add To Wishlist Part 3

--- app/Http/Controllers/Frontend/CartController.php
+++ app/Http/Controllers/Frontend/CartController.php
@@ -79,26 +79,32 @@ class CartController extends Controller
 
 		public function AddToWishlist(Request $request, $product_id){
 			
-			if(Auth::check())
-			{
-
+			if(Auth::check()) {
 				$exists = Wishlist::where('user_id', Auth::id())->where('product_id', $product_id)->first();
 
-				Wishlist::insert([
-					'user_id' => Auth::id(),
-					'product_id' => $product_id,
-					'created_at' => Carbon::now(),
-				]);
+				if (!$exists){
+					Wishlist::insert([
+						'user_id' => Auth::id(), 
+						'product_id' => $product_id, 
+						'created_at' => Carbon::now(), 
+					]);
+					
+					return response()->json(['success' => 'Successfully Added On Your Wishlist']);
+				
+				}else{
 
-			return response()->json(['success' => 'Successfully Added On Your Wishlist']);
+					return response()->json(['error' => 'This product already added to your Wishlist']);
 
+				}
 
-			}else
+			}else{
 
 			return response()->json(['error' => 'At first login your account']);
 
 			}
-}
+		}
+	}
+
 
 
 
