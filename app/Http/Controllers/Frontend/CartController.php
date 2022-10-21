75a7e57a45483989ac392f05b2aa3f9872f3fcd1
afiq <afiqvdx20@gmail.com>
2022-10-21 21:07:07 +0800

    Coupon Apply With Ajax part 4

--- app/Http/Controllers/Frontend/CartController.php
+++ app/Http/Controllers/Frontend/CartController.php
@@ -115,7 +115,7 @@ class CartController extends Controller
 				Session::put('coupon',[
 					'coupon_name' => $coupon->coupon_name,
 					'coupon_discount' => $coupon->coupon_discount,
-					'discount_amount' =>  round(Cart::total() * $coupon->coupon_discount/100,),
+					'discount_amount' => Cart::total() * $coupon->coupon_discount/100, 
 					'total_amount' => round(Cart::total() - Cart::total() * $coupon->coupon_discount/100)
 					
 				]);
--- resources/views/frontend/main_master.blade.php
+++ resources/views/frontend/main_master.blade.php
@@ -650,10 +650,47 @@ function cartDecrement(rowId){
 			url: "{{ url('/coupon-calculation')}}",
 			dataType: 'json',
 			success:function(data){
-				
+				if (data.total){
+					$('#couponCalField').html(
+						`<tr>
+                <th>
+                    <div class="cart-sub-total">
+                        Subtotal<span class="inner-left-md">$ ${data.total}</span>
+                    </div>
+                    <div class="cart-grand-total">
+                        Grand Total<span class="inner-left-md">$ ${data.total}</span>
+                    </div>
+                </th>
+            </tr>`
+				)
+
+				} else{
+					$('#couponCalField').html(
+						`<tr>
+        <th>
+            <div class="cart-sub-total">
+                Subtotal<span class="inner-left-md">$ ${data.subtotal}</span>
+            </div>
+            <div class="cart-sub-total">
+                Coupon<span class="inner-left-md">$ ${data.coupon_name}</span>
+                <button type="submit" onclick="couponRemove()"><i class="fa fa-times"></i>  </button>
+            </div>
+             <div class="cart-sub-total">
+                Discount Amount<span class="inner-left-md">$ ${data.discount_amount}</span>
+            </div>
+            <div class="cart-grand-total">
+                Grand Total<span class="inner-left-md">$ ${data.total_amount}</span>
+            </div>
+        </th>
+            </tr>`
+				)
+
+				}
 			}
-		})
+		});
+	
 	}
+	couponCalculation();
   </script>
   
   
