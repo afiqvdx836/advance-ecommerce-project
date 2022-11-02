970883e1f8df82d46f37dc142ce23a760825a53a
afiq <afiqvdx20@gmail.com>
2022-11-02 11:56:39 +0800

    Read Orders In User Profile Order pages

--- app/Http/Controllers/User/AllUserController.php
+++ app/Http/Controllers/User/AllUserController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers\User;
 
-use App\Http\Controllers\Controller;
+use App\Models\Order;
 use Illuminate\Http\Request;
-
+use App\Http\Controllers\Controller;
+use Auth;
 class AllUserController extends Controller
 {
     public function MyOrders(){
