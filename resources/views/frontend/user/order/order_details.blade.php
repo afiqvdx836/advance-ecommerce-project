ad6dc443b40ac2d9c1ed5c0145d9e5ba5171da49
afiq <afiqvdx20@gmail.com>
2022-11-03 15:19:55 +0800

    View Users Order In Profile Part 3

--- resources/views/frontend/user/order/order_details.blade.php
+++ resources/views/frontend/user/order/order_details.blade.php
@@ -61,11 +61,142 @@
                     
                 </div>
             
+            </div><!-- End col-md-5-->
+
+            <div class="col-md-5">
+            
+                <div class="card">
+                    <div class="card-header"><h4>Order Details</h4> </div>
+                    <hr>
+                    <div class="card-body" style="background: #E9EBEC;">
+                        <table class="table">
+                            <tr>
+                                <th> Name : </th>
+                                <th>{{ $order->user->name }}</th>
+                            </tr>
+
+                            <tr>
+                                <th> Phone :</th>
+                                <th>{{ $order->user->phone }}</th>
+                            </tr>
+
+                            <tr>
+                                <th> Payment Type :</th>
+                                <th>{{$order->payment_type}}</th>
+                            </tr>
+
+                            <tr>
+                                <th> Transaction ID: </th>
+                                <th>{{$order->transaction_id}}</th>
+                            </tr>
+
+                            <tr>
+                                <th> Invoice :</th>
+                                <th class="text-danger"> {{ $order->invoice_no }} </th>
+
+                            </tr>
+
+                            <tr>
+                                <th> Order Total :</th>
+                                <th>{{ $order->amount }} </th>
+                            </tr>
+
+                            <tr>
+                                <th> Order : </th>
+                                <span class="badge badge-pill badge-warning" style="background: #418DB9;">{{ $order->status }} </span> </th>
+                            </tr>
+
+                           
+                            
+                        </table>
+                    </div>
+                    
+                </div>
+            
             </div><!-- End col-md-8-->
-
-
 			
 			
+		</div> <!-- // end row -->
+
+        <div class="row">
+			
+			
+
+
+
+			<div class="col-md-12">
+            <div class="table-responsive">
+                <table class="table">
+                    <tbody>
+                        <tr style="background: #e2e2e2;">
+                            <td class="col-md-1">
+                                <label for=""> Image </label>
+                            </td>
+
+                            <td class="col-md-3">
+                                <label for=""> Product name </label>
+                            </td>
+
+                            <td class="col-md-3">
+                                <label for=""> Product Code </label>
+                            </td>
+
+                            <td class="col-md-2">
+                                <label for=""> Color </label>
+                            </td>
+
+                            <td class="col-md-2">
+                                <label for=""> Size </label>
+                            </td>
+
+                            <td class="col-md-1">
+                                <label for=""> Quantity </label>
+                            </td>
+
+                            <td class="col-md-1">
+                                <label for=""> Price </label>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+             
+            @foreach($orderItem as $item)
+            <tr>
+                     <td class="col-md-1">
+                       <label for=""><img src="{{ asset($item->product->product_thambnail) }}" height="50px;" width="50px;"> </label>
+                     </td>
+     
+                     <td class="col-md-3">
+                       <label for=""> {{ $item->product->product_name_en }}</label>
+                     </td>
+     
+     
+                      <td class="col-md-3">
+                       <label for=""> {{ $item->product->product_code }}</label>
+                     </td>
+     
+                     <td class="col-md-2">
+                       <label for=""> {{ $item->color }}</label>
+                     </td>
+     
+                     <td class="col-md-2">
+                       <label for=""> {{ $item->size }}</label>
+                     </td>
+     
+                      <td class="col-md-2">
+                       <label for=""> {{ $item->qty }}</label>
+                     </td>
+     
+               <td class="col-md-2">
+                       <label for=""> ${{ $item->price }}  ( $ {{ $item->price * $item->qty}} ) </label>
+                     </td>
+     
+                   </tr>
+                   @endforeach
+            </div><!-- End col-md-5-->
+
+           
 		</div> <!-- // end row -->
 		
 	</div>
