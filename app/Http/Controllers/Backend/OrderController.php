365a4aed0d7725dcde2bf59ce2d274ababf4a469
afiq <afiqvdx20@gmail.com>
2022-11-10 14:44:05 +0800

    Read Orders By Status

--- app/Http/Controllers/Backend/OrderController.php
+++ app/Http/Controllers/Backend/OrderController.php
@@ -19,4 +19,45 @@ class OrderController extends Controller
         $orderItem = OrderItem::with('product')->where('order_id',$order_id)->get();
         return view ('backend.orders.pending_order_details', compact('order','orderItem'));
     }
+
+    
+    //comfirm orders
+    public function ConfirmedOrders(){
+        $orders = Order::where('status', 'confirmed')->orderBy('id','DESC')->get();
+        return view ('backend.orders.confirmed_orders', compact('orders'));
+    }
+
+
+    //processnig orders
+    public function ProcessingOrders(){
+        $orders = Order::where('status', 'processing')->orderBy('id','DESC')->get();
+        return view ('backend.orders.processing_orders', compact('orders'));
+    }
+
+
+    //picked orders
+    public function PickedOrders(){
+        $orders = Order::where('status', 'picked')->orderBy('id','DESC')->get();
+        return view ('backend.orders.picked_orders', compact('orders'));
+    }
+
+
+    //Shipped orders
+    public function ShippedOrders(){
+        $orders = Order::where('status', 'confirmed')->orderBy('id','DESC')->get();
+        return view ('backend.orders.shiped_orders', compact('orders'));
+    }
+
+
+    //comfirm orders
+    public function DeliveredOrders(){
+        $orders = Order::where('status', 'delivered')->orderBy('id','DESC')->get();
+        return view ('backend.orders.delivered_orders', compact('orders'));
+    }
+
+    //canceled orders
+    public function CancelOrders(){
+        $orders = Order::where('status', 'cancel')->orderBy('id','DESC')->get();
+        return view ('backend.orders.cancel_orders', compact('orders'));
+    }
 }
