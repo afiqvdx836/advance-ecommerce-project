e2b22449c8abf4761b1964c49c6f4b05d66a51e4
afiq <afiqvdx20@gmail.com>
2022-12-07 16:08:40 +0800

    Add count function

--- resources/views/backend/user/all_user.blade.php
+++ resources/views/backend/user/all_user.blade.php
@@ -18,7 +18,7 @@
 
 			 <div class="box">
 				<div class="box-header with-border">
-				  <h3 class="box-title">All User </h3>
+				  <h3 class="box-title">All User <span class="badge badge-pill badge-danger"> {{ count($users) }}</span> </h3>
 				</div>
 				<!-- /.box-header -->
 				<div class="box-body">
@@ -45,7 +45,7 @@
 
 		<td>
 			@if ($user->UserOnline())
-				<span class="badge badge-pill badge-success">Active Now</span>
+				<span class="badge badge-pill badge-success">Active Now </span>
 			@else
 			<span class="badge badge-pill badge-danger">{{Carbon\Carbon::parse($user->last_seen)->diffForHumans() }}</span>
 			@endif
